57263819c423db1706bc9ce9fa1f570b1ed1b123
Ubuntu <azureuser@caohy-2.caohy-2.b7.internal.cloudapp.net>
2015-09-11 16:13:16 +0000

    count.py

--- count.py
+++ count.py
@@ -7,10 +7,14 @@ def count(file, key):
         reader = csv.reader(f)
         try:
             for num, row in enumerate(reader):
+                phrase_key = 0
                 for item in row:
                     item = item.lower()
                     if item.count(key):
-                        count_key = count_key + 1
+                        phrase_key = phrase_key + 1
+                        print phrase_key
+                if phrase_key:
+                    count_key = count_key + 1
             return count_key
         except csv.Error as e:
             sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
