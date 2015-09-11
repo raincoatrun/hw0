1ab98e71dbab559b038596d09e790ef7d9d4611c
Ubuntu <azureuser@caohy-2.caohy-2.b7.internal.cloudapp.net>
2015-09-11 06:22:01 +0000

    count.py

---
+++ count.py
@@ -0,0 +1,20 @@
+import sys
+import csv
+
+def count(file, key):
+    count_key = 0
+    with open(file, mode='r') as f:
+        reader = csv.reader(f)
+        try:
+            for num, row in enumerate(csv.reader(f)):
+                for item in row:
+                    item = item.lower()
+                    if item.count(key):
+                        count_key = count_key + 1
+            return count_key
+        except csv.Error as e:
+            sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+
+print count('iowa-liquor-sample.csv','single malt scotch')
+
+    
